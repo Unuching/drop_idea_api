5e7f4f6383e6bb544cb508e3bb70fe210303d132
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-10 22:08:31 +0600

    Auto: 1 added, 2 modified - 2025-09-10

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+// @route             GET  /api/ideas
+// @description       get all ideas
+//@access             public
+
 router.get('/', (req, res) => {
   const ideas = [
     { id: 1, title: 'Idea 1', description: 'This is an Idea 1' },
@@ -9,8 +13,15 @@ router.get('/', (req, res) => {
     { id: 3, title: 'Idea 3', description: 'This is an Idea 3' },
   ];
 
+  res.status(400);
+  throw new Error('Ok my bad sorry');
+
   res.json(ideas);
 });
+
+// @route             POST  /api/ideas
+// @description       create new idea
+//@access             public
 
 router.post('/', (req, res) => {
   const { title, description } = req.body;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import router from './routes/ideaRoutes.js';
+import ideaRouter from './routes/ideaRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -11,6 +12,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use('/api/ideas', ideaRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`not found anything- ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
