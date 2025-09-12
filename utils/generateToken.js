49218158a275597c9c1982df44ffbf23033d91c4
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-12 22:44:43 +0600

    Auto: 1 added, 1 modified - 2025-09-12

---
+++ utils/generateToken.js
@@ -0,0 +1,14 @@
+import { SignJWT } from 'jose';
+import { JWT_SECRET } from './getJWTSecret.js';
+
+// generates a JWT token
+// @param {onject} payload -- Data to embed in token
+// @param {string} expiresIn - Expiration time(ex - "15 min", '7d')
+
+export const generateToken = async (payload, expiresIn = '15m') => {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(expiresIn)
+    .sign(JWT_SECRET);
+};
