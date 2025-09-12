88a65cfb347c1afd4c72318a914375b20537a24a
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-12 20:00:09 +0600

    Auto: 1 modified - 2025-09-12

--- models/User.js
+++ models/User.js
@@ -3,6 +3,11 @@ import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -26,4 +31,17 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+//Password hashing
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
+
+export default User;
