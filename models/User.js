db3419f14e03be0ccf52b6fbbddd77893a1cb57c
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-12 08:18:29 +0600

    Auto: 1 added, 2 modified - 2025-09-12

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowecase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
